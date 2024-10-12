2d2ea18bc98ba871b0858f411a42cd9369ac0da7
Maulana-king <agusrohim56@gmail.com>
2024-10-12 18:02:14 +0700

    reset all code html, js and css to empty

--- eSketch.js
+++
@@ -1,9 +0,0 @@
-
-
-const container = document.querySelector('.container');
-
-for(let i = 1; i <= 256; i++) {
-    let makeDiv = document.createElement('div');
-    //makeDiv.innerText = i;
-    container.appendChild(makeDiv);
-}
