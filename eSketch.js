06ef9bb3dd8b6e9fd77d20445635dae99d8923ea
Maulana-king <agusrohim56@gmail.com>
2024-10-13 07:10:06 +0700

    create element button, input form and container for them

---
+++ eSketch.js
@@ -0,0 +1,30 @@
+//ADD BUTTON
+const borderContainer = document.querySelector('.border-container');
+const button = document.createElement('button');
+button.innerText = "Change Grid";
+button.classList.add('change-grid');
+
+//ADD FORM INPUT
+const formInput = document.createElement('form');
+const inputField = document.createElement('input');
+inputField.setAttribute('type', 'text');
+inputField.setAttribute('placeholder', 'Max Grid = 100');
+formInput.appendChild(inputField);
+
+//ADD CONTAINER BUTTON & INPUT
+const smallContainer = document.createElement('div');
+smallContainer.classList.add('small-container');
+borderContainer.insertAdjacentElement('beforebegin', smallContainer);
+smallContainer.appendChild(formInput);
+smallContainer.appendChild(button);
+
+const container = document.querySelector('.container');
+
+//Create a grid box
+// for(let i = 0; i < (16 * 16); i++) {
+//     const createDiv = document.createElement('div');
+//     createDiv.classList.add('boxs');
+//     container.appendChild(createDiv);
+// }
+
+
